d275317cf098b3022c44c519bdbe3fc3dd4e8f5d
Aziz Namazov <azee@77.88.2.103-spb.dhcp.yndx.net>
2015-02-10 21:36:14 +0300

    Remove readme

---
+++ src/main/webapp/js/models/project/ProjectModel.js
@@ -0,0 +1,42 @@
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'libs/ginny/ginny'
+], function($, _, Backbone){
+
+    var SomeBeanModel = Backbone.Model.extend({
+        id: "",
+        url : 'mockdata/someBeanData.json',
+
+        methodToURL: {
+            'read': "",
+            'create': "mockdata/someBeanData.json",
+            'update': "mockdata/someBeanData.json",
+            'delete': ""
+        },
+
+        initialize: function (options) {
+            if (options != null) {
+                this.id = options.id;
+                this.methodToURL['read'] = this.url + "?id=" + this.id;
+                this.methodToURL['delete'] = this.url + "?id=" + this.id;
+            }
+        },
+
+        sync: function(method, model, options) {
+            options = options || {};
+            options.url = model.methodToURL[method.toLowerCase()];
+
+            return Backbone.sync(method, model, options);
+        },
+
+        parse: function(data){
+            data.displayCreatedTime = Ginny.timeToDate(data.created);
+            data.displayTimePassed = Ginny.timePassed(data.time);
+            return data;
+        }
+    });
+
+    return SomeBeanModel;
+});
